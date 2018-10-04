e33b5503913434ac9136afceae34d457e871453c
sas <sas@tradebyte.biz>
2018-10-04 09:22:03 +0200

    changed tag row length

--- wiki/page/views.py
+++ wiki/page/views.py
@@ -31,7 +31,7 @@ def index(request):
 			SHORT_DESCRIPTION_LENGTH
 			)
 
-	render_params = {"greeting" : "Mer haba", "pages" : pages, 'wip_pages' : wip_pages}
+	render_params = {"greeting" : "Velkommen", "pages" : pages, 'wip_pages' : wip_pages}
 	return render(request, template_name, render_params)
 
 def show_search_list(request):
